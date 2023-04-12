c0be3fc66250b522a9a4f5d614589520c18545d5
Leontiy Tsukanov <122561265+LeontiyTsukanov@users.noreply.github.com>
2023-04-12 10:10:40 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,11 +1,11 @@
 import pandas as pd
 import numpy as np
-from scipy.stats import ttest_ind
+from scipy.stats import mannwhitneyu
 
 chat_id = 407415686 # Ваш chat ID, не меняйте название переменной
 
 def solution(x, y) -> bool: # Одна или две выборке на входе, заполняется исходя из условия
     flag = False
-    if ttest_ind(x, y, equal_var=False, alternative="less").pvalue >= 0.01:
+    if mannwhitneyu(x, y, alternative="less").pvalue >= 0.01:
         flag = True
     return flag # Ваш ответ, True или False
